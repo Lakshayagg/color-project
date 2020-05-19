5a42b2610a36b333933fa183c681f3fe553e9a6b
Lakshay Aggarwal <Lakshayagg5@gmail.com>
2020-05-20 01:40:33 +0530

    add Palette

---
+++ src/Palette.js
@@ -0,0 +1,10 @@
+import React, { component, Component } from 'react';
+
+class Palette extends Component {
+    render() {
+        return (
+            <div><h1>Palette</h1></div>
+        );
+    }
+}
+export default Palette;
