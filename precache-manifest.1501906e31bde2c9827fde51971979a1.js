1a7d1baa8f1f952868baad59921b7452837a130e
Lakshay Aggarwal <Lakshayagg5@gmail.com>
2020-05-20 00:51:37 +0530

    Updates

---
+++ precache-manifest.1501906e31bde2c9827fde51971979a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65bf44ec985d5196372dfaa08021eb54",
+    "url": "/color-project/index.html"
+  },
+  {
+    "revision": "35f698330761a297280f",
+    "url": "/color-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc93f8f4ef6dedd3aa6b",
+    "url": "/color-project/static/js/2.a52b9820.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-project/static/js/2.a52b9820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f698330761a297280f",
+    "url": "/color-project/static/js/main.04d3d1e3.chunk.js"
+  },
+  {
+    "revision": "77bd140bb7260cb6d7db",
+    "url": "/color-project/static/js/runtime-main.4340b39a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/color-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
